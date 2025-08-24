01c2714e4e605120506747ad83d010d72de530ba
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-08-24 16:08:50 +0600

    Add Swiper integration with image banners and update CSS structure

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // allows clicking the dots
+  },
+});
